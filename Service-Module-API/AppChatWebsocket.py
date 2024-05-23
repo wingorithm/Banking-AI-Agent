43bf687c337d5e1ecdfb640a0f3111655a01aa0e
CalvinnnK <101546535+CalvinnnK@users.noreply.github.com>
2024-05-23 23:44:51 +0700

    api call to android

--- Service-Module-API/AppChatWebsocket.py
+++ Service-Module-API/AppChatWebsocket.py
@@ -22,14 +22,16 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
     try:
         while True:
+            # data = await websocket.receive_json()
             data = await websocket.receive_json()
+            print("Here's my Data -> ", data)
             user_message = generalResponse(data["message"], data["role"])
-            await manager.send_message(user_message, websocket)
-
+            print("Here's my Data -> ", user_message.message , " <> ", user_message.role)
+            # await manager.send_message(user_message, websocket)
             # TODO: sebelum response preprocessing dulu
             respose = await retrieve.getRespond(data, client_id)
             await manager.broadcast(respose, websocket)
             
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        await manager.broadcast2(f"Client #{client_id} left the chat")
--- Service-Module-API/service/ConnectionManagerService.py
+++ Service-Module-API/service/ConnectionManagerService.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from model.response.GeneralResponse import generalResponse
+import json
 
 class connectionManager:
     def __init__(self):
         self.active_connections: list[WebSocket] = []
 
     async def connect(self, websocket: WebSocket):
-        print(websocket)
+        print(websocket, " Hello")
         await websocket.accept()
         self.active_connections.append(websocket)
 
@@ -14,9 +15,18 @@ class connectionManager:
         self.active_connections.remove(websocket)
 
     async def send_message(self, data: generalResponse, websocket: WebSocket):
+        print("Data Berhasil bro ", data)
         await websocket.send_text(f"{data.role}: {data.message}")
 
     async def broadcast(self, data: generalResponse, websocket: WebSocket):
-        await websocket.send_text(f"{data.role}: {data.message}")
-        # for connection in self.active_connections:
-        #     await connection.send_text(f"{data.role}: {data.message}")
+        print("Data Berhasil bro broadcast", data.message, "role :", data.role)
+        data2 = {
+            "message": data.message,
+            "role" : data.role
+        }
+        json_string = json.dumps(data2)
+        await websocket.send_text(f"{json_string}")
+        
+    async def broadcast2(self, data: generalResponse):
+        for connection in self.active_connections:
+            await connection.send_text(f"{data.role}: {data.message}")
